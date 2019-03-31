3768683e38a03d3c8a63406efb011e9fe84a1bbc
Federico Cucino <24813315+turbostar190@users.noreply.github.com>
2019-03-31 18:59:09 +0200

    Updated player values based on patch 1.3

--- SpeedPlayer.lua
+++ SpeedPlayer.lua
@@ -8,7 +8,6 @@
 SpeedPlayer = {}
 
 function SpeedPlayer:loadMap(...)
-	-- 						 GDN says 6.0...
 	self.SPEEDS = {0.8, 2.0, 4.0*0.7, 4.0, 12.0, 32.0, 60.0, 80.0} -- m/s
 	self.SPEEDSLENGTH = table.getn(self.SPEEDS)
 	self.TEXTS = {[0.8] = "keyslow3", [2.0] = "keyslow2", [4.0*0.7] = "keyslow1", [4.0] = "key0", [12.0] = "key1", [32.0] = "key2", [60.0] = "key15", [80.0] = "key3", ["other"] = "othermod"}
@@ -35,6 +34,7 @@ end
 function SpeedPlayer:reduceSpeed()
 	if (self.cont == 1) then return end
 	self.cont = self.cont - 1
+	-- g_inputBinding.events[SpeedPlayer.eventIdReduce].callbackState is -1 here
 	
 	local spe = self.SPEEDS[self.cont]
 	SpeedPlayer:setSpeed(spe)
@@ -42,6 +42,7 @@ end
 function SpeedPlayer:incrementSpeed()
 	if (self.cont == self.SPEEDSLENGTH) then return end
 	self.cont = self.cont + 1
+	-- g_inputBinding.events[SpeedPlayer.eventIdIncrease].callbackState is 1 here
 	
 	local spe = self.SPEEDS[self.cont]
 	SpeedPlayer:setSpeed(spe)
@@ -51,10 +52,10 @@ function SpeedPlayer:setSpeed(speed)
 	local info = g_currentMission.player.motionInformation
 	if speed ~= nil then 
 		info.maxWalkingSpeed = tonumber(speed)
-		info.maxRunningSpeed = tonumber(speed * 1.5)
-		info.maxSwimmingSpeed = tonumber(speed / 2)
-		info.maxCrouchingSpeed = tonumber(speed / 3)
-		info.maxFallingSpeed = tonumber(speed)
+		info.maxRunningSpeed = tonumber(speed * (9/4))
+		info.maxSwimmingSpeed = tonumber(speed / (4/3))
+		info.maxCrouchingSpeed = tonumber(speed / 2)
+		info.maxFallingSpeed = tonumber(speed * 1.5)
 		info.maxCheatRunningSpeed = tonumber(speed * (34/4))
 	end
 end
@@ -73,7 +74,6 @@ function SpeedPlayer:update(dt)
 			
 			local eventIdReduce = self.eventIdReduce
 			local eventIdIncrease = self.eventIdIncrease
-			
 			g_inputBinding:setActionEventActive(eventIdReduce, SpeedPlayer.cont ~= 1)
 			g_inputBinding:setActionEventTextVisibility(eventIdReduce, SpeedPlayer.cont ~= 1)
 			g_inputBinding:setActionEventActive(eventIdIncrease, SpeedPlayer.cont ~= SpeedPlayer.SPEEDSLENGTH)
